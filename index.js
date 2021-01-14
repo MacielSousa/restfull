3e4ac2c29c630b248c0fe42c5a1acbe3421db69f
Maciel Sousa <fmacielsousa@gmail.com>
2021-01-14 14:45:46 -0300

    Validando dados com Express Validator

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const consign = require('consign');
 const bodydParser = require('body-parser');
+const expressValidator = require('express-validator');
 
 let app = express();
 
+
 app.use(bodydParser.urlencoded({extended: false}));
 app.use(bodydParser.json());
+app.use(expressValidator());
 
 consign().include('routes').include('utils').into(app);
 
